642a279e3ff7c487f64944b32aeece4020dcef3d
mohamed raai <mohamedraai41@gmail.com>
2021-01-18 23:49:26 +0200

    add function to js file

--- project landing page/js/app.js
+++ project landing page/js/app.js
@@ -11,30 +11,53 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
-
+ */
+const fragment = document.createDocumentFragment();
+const sectionList = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
+function createNavItemHTML(id, name) {
+    const itemHTML = `<a class ="menu__link" data-id="${id}">${name}</a>`;
+    return itemHTML;
+}
 
-
+function isInViewport(elem) {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
-
+function buildNavigation() {
+    for (let i = 0; i < sectionList.length; i++) {
+        const newMenuItem = document.createElement('li');
+        const sectionName = sectionList[i].getAttribute('data-nav')
+        const sectionId = sectionList[i].getAttribute('id')
+        newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
+        fragment.appendChild(newMenuItem);
+    }
+    const navBarList = document.getElementById('navbar__list')
+    navBarList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -46,9 +69,9 @@
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
-// Build menu 
+
 
 // Scroll to section on link click
 
